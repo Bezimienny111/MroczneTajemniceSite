e63b0872d23f0e908ae4a6e8e9a303f89aece93f
Robert Kowalski <bezijuve@gmail.com>
2026-04-03 15:25:08 +0200

    Deploy website - based on 9ef8cc15009b313198b7b436171dd6d06d1747e6

--- assets/js/6e1737a7.3629c132.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkbezimienny_111_github_io=globalThis.webpackChunkbezimienny_111_github_io||[]).push([[7390],{4351(e){e.exports=JSON.parse('{"metadata":{"permalink":"/newsy","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
